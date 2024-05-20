3ce33af6a021140771efdbc0cf0f238cbabcf18c
s0ru <apazgarcia23@outlook.es>
2024-05-20 20:39:11 +0200

    abstract wave elevation function

--- src/shaders/water/vertex.glsl
+++ src/shaders/water/vertex.glsl
@@ -14,20 +14,26 @@ varying vec3 vPosition;
 
 #include ../includes/perlinClassic3D.glsl;
 
+float waveElevation(vec3 position){
+    float elevation = sin(position.x * uBigWavesFrequency.x + uTime * uBigWavesSpeed) *
+                      sin(position.z * uBigWavesFrequency.y + uTime * uBigWavesSpeed) *
+                      uBigWavesElevation;
+
+    for(float i = 1.0; i <= uSmallIterations; i++)
+    {
+        elevation -= abs(perlinClassic3D(vec3(position.xz * uSmallWavesFrequency * i, uTime * uSmallWavesSpeed)) * uSmallWavesElevation / i);
+    }
+
+    return elevation;
+}
+
 void main()
 {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
     // Elevation
-    float elevation = sin(modelPosition.x * uBigWavesFrequency.x + uTime * uBigWavesSpeed) *
-                      sin(modelPosition.z * uBigWavesFrequency.y + uTime * uBigWavesSpeed) *
-                      uBigWavesElevation;
-
-    for(float i = 1.0; i <= uSmallIterations; i++)
-    {
-        elevation -= abs(perlinClassic3D(vec3(modelPosition.xz * uSmallWavesFrequency * i, uTime * uSmallWavesSpeed)) * uSmallWavesElevation / i);
-    }
     
+    float elevation = waveElevation(modelPosition.xyz);
     modelPosition.y += elevation;
 
     vec4 viewPosition = viewMatrix * modelPosition;
